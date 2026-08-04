76476629cdbb4d6ee80b9ee2d27c567930d9d7b5
Arun Kumar <arunjadiya681990@gmail.com>
2026-08-04 22:13:23 +0530

    new_rgadded

--- provider.tf
+++ provider.tf
@@ -15,3 +15,9 @@ resource "azurerm_resource_group" "myrg1" {
     name = "arun_rg"
     location = "eastus"
 }
+
+resource "azurerm_resource_group" "myrg2" {
+    name = "arun_rg1"
+    location = "eastus"
+}
+
